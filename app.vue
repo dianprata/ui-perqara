ba64b6d34350c17442fd51e128619b359a2c95c9
Dian Pratama <dian@perqara.com>
2024-07-15 12:17:21 +0700

    fix: nuxt config

--- app.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="p-5 flex gap-4 items-end">
-    <UIButton size="lg">Test</UIButton>
-    <UIButton>Test</UIButton>
-    <UIButton size="sm">Test</UIButton>
-    <UIButton size="xs">Test</UIButton>
-    <UIButton size="icon">A</UIButton>
-  </div>
-</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,20 +5,17 @@ const currentDir = dirname(fileURLToPath(import.meta.url))
 
 export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss'],
-
   components: [
     {
       path: join(currentDir, './components/ui'),
       // this is required else Nuxt will autoImport `.ts` file
       extensions: ['.vue'],
       // prefix for your components, eg: UiButton
-      prefix: 'UI'
+      prefix: ''
     },
   ],
-
   tailwindcss: {
     cssPath: join(currentDir, './assets/css/tailwind.css'),
   },
-
   compatibilityDate: '2024-07-15',
 })
