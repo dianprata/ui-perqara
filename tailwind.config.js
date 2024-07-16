27356e7b467180000916ea35d9996483989ebf30
Dian Pratama <dian@perqara.com>
2024-07-16 18:26:13 +0700

    fix: tailwindcss-animate not found

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,16 @@
-const animate = require("tailwindcss-animate")
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
   safelist: ["dark"],
   prefix: "",
-  
+
   content: [
     './pages/**/*.{ts,tsx,vue}',
     './components/**/*.{ts,tsx,vue}',
     './app/**/*.{ts,tsx,vue}',
     './src/**/*.{ts,tsx,vue}',
 	],
-  
+
   theme: {
     container: {
       center: true,
@@ -89,5 +87,5 @@ module.exports = {
       },
     },
   },
-  plugins: [animate],
+  plugins: [[require("tailwindcss-animate")]],
 }
